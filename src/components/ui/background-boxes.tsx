a8ed882dd50b55afd7ac4030cbfd73aecd23a72d
Leonardo Araujo Sarmento <leosarmento@ucl.br>
2024-08-05 20:33:44 -0300

    fix(): aceternity background boxes optimization

--- src/components/ui/background-boxes.tsx
+++ src/components/ui/background-boxes.tsx
@@ -3,8 +3,8 @@ import { motion } from 'framer-motion';
 import { cn } from '@lib/utils';
 
 export const BoxesCore = ({ className, ...rest }: { className?: string }) => {
-  const rows = new Array(150).fill(1);
-  const cols = new Array(100).fill(1);
+  const rows = new Array(50).fill(1);
+  const cols = new Array(55).fill(1);
   let colors = [
     '--sky-300',
     '--pink-300',
